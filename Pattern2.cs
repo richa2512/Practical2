f629a7e5cce8f057b1eb2a3470d13a2f2854c505
richa2512 <41030592+richa2512@users.noreply.github.com>
2019-02-08 13:13:12 +0530

    Pattern2

---
+++ Pattern2.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for(i=1;i<5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    Console.Write(j+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
