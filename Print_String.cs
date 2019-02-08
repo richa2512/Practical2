34f1c8e68c92963e2af2df661d7ee4e75a4ea2bf
richa2512 <41030592+richa2512@users.noreply.github.com>
2019-02-08 13:11:27 +0530

    Print String

---
+++ Print_String.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Country");
+            string country = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " from country " +  country);
+            Console.ReadLine();
+            
+        }
+    }
+}
